480ef6945a65276dff683c3968644bffead2df35
Ethan Krich <ektechworld@gmail.com>
2025-05-08 15:30:41 -0700

    chore: add husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "eslint/config";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig({
   plugins: {
     pluginJs: pluginJs.configs.recommended,

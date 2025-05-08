91e48caf9bbb2aaa3c631966cfcf3a5446324217
Ethan Krich <ektechworld@gmail.com>
2025-05-08 14:48:19 -0700

    chore: add basic config

---
+++ .alexrc.cjs
@@ -0,0 +1,26 @@
+//@ts-check
+
+module.exports.allow = [
+  "attacks",
+  "color",
+  "dead",
+  "execute",
+  "executed",
+  "executes",
+  "execution",
+  "executions",
+  "failed",
+  "failure",
+  "failures",
+  "fire",
+  "fires",
+  "hook",
+  "hooks",
+  "host-hostess",
+  "invalid",
+  "remains",
+  "special",
+  "white"
+];
+
+module.exports.profanitySureness = 1;
---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig({
+  plugins: {
+    pluginJs: pluginJs.configs.recommended,
+    tseslint
+  },
+  rules: {
+    "prefer-const": "warn",
+    "no-var": "warn"
+  }
+});

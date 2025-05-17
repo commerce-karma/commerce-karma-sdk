e4224f3d0b9896c050fc5a1c839c5438f0e82672
Ethan Krich <ektechworld@gmail.com>
2025-05-17 12:56:03 -0700

    chore: prepare for npm publish

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const commerceKarmaURL = "http://localhost:3000";
+export const commerceKarmaURL = "https://commerce-karma.vercel.app";
 export const commerceKarmaBackendURL = "https://7o1h60hh44.execute-api.us-east-1.amazonaws.com";
 
 export { default as authorize } from "./authorize";
